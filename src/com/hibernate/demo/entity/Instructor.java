598b58defe212bb338b8ad484e6bd8d6638019f2
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-09 21:02:38 +0000

    Update Instructor Class
    Demo

--- src/com/hibernate/demo/entity/Instructor.java
+++ src/com/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -41,6 +45,9 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(mappedBy="instructor", cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+	private List<Course> courses;
 	
 	public Instructor() {
 		
@@ -98,5 +105,24 @@ public class Instructor {
 				+ ", instructorDetail=" + instructorDetail + "]";
 	}
 	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	//CONVENIENCE METHOD FOR BI-DIRECTIONAL
+	public void add(Course tempCourse) {
+		
+		if (this.courses == null) {
+			this.courses = new ArrayList<>();
+		}
+		
+		this.courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
 	
 }
